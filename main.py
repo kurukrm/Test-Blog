fae904ca10f57ff37f8efeaf77e7ef52470e7afd
kurukrm <kkuru.kerem@gmail.com>
2023-03-24 13:43:44 +0300

    Config

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ["app_config"]
+app.config['SECRET_KEY'] = os.environ.get("app_config")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app,
